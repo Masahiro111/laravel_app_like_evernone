87bf0c61cbf9510e16f81bc794fdf2071411d05a
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-05-11 01:34:14 +0900

    URLのルーティングを書こう

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login.index');
+
+Route::get('/user', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('user.register');
+
+Route::get('/memo', function () {
+    return view("memo");
+})->name('memo.index');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('/memo', 'MemoController@index')->name('memo.index');
