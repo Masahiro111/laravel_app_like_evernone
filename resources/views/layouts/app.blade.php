6af5e3face51f5fda5e0904ed14b42a68dde710e
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-05-12 02:29:58 +0900

    add tailwind cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <style>
         /*--全体共通--*/
         html {
--- resources/views/memo.blade.php
+++ resources/views/memo.blade.php
@@ -9,8 +9,24 @@
                     xxxさん、こんにちは。
                 </div>
                 <div class="pr-1">
-                    <a href="{{ route('memo.add') }}" class="btn btn-success"><i class="fas fa-plus"></i></a>
-                    <a href="{{ route('login.index') }}" class="btn btn-dark"><i class="fas fa-sign-out-alt"></i></a>
+                    <a href="{{ route('memo.add') }}" class="btn btn-success">
+                        <i class="fas fa-plus">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                            </svg>
+                        </i>
+                    </a>
+                    <a href="{{ route('login.index') }}" class="btn btn-dark">
+                        <i class="fas fa-sign-out-alt">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                            </svg>
+                        </i>
+                    </a>
                 </div>
             </div>
             <div class="left-memo-title h3 pl-3 pt-3">
@@ -32,8 +48,24 @@
             <form class="w-100 h-100" method="post">
                 <input type="hidden" name="edit_id" value="" />
                 <div id="memo-menu">
-                    <button type="button" class="btn btn-danger" formaction=""><i class="fas fa-trash-alt"></i></button>
-                    <button type="button" class="btn btn-success" formaction=""><i class="fas fa-save"></i></button>
+                    <button type="button" class="btn btn-danger" formaction="">
+                        <i class="fas fa-trash-alt">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                        </i>
+                    </button>
+                    <button type="button" class="btn btn-success" formaction="">
+                        <i class="fas fa-save">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+                            </svg>
+                        </i>
+                    </button>
                 </div>
                 <input type="text" id="memo-title" name="edit_title" placeholder="タイトルを入力する..." value="" />
                 <textarea id="memo-content" name="edit_content" placeholder="内容を入力する..."></textarea>
