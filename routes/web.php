ccffb751782f446840e854b7faa600929630ce5c
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-05-11 19:39:40 +0900

    finished part 6-2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,16 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login.index');
-
-Route::get('/user', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('user.register');
-
-Route::post('/user/register', [App\Http\Controllers\Auth\RegisterController::class, 'register'])->name('user.exec.register');
-
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('login.index');
+Route::get('/user', [RegisterController::class, 'showRegistrationForm'])->name('user.register');
+Route::post('/user/register', [RegisterController::class, 'register'])->name('user.exec.register');
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('/memo', function () {
-        return view("memo");
-    })->name('memo.index');
+    Route::get('/memo', [MemoController::class, 'index'])->name('memo.index');
+    Route::get('/memo/add', [MemoController::class, 'add'])->name('memo.add');
 });
 Auth::routes();
 
